0bd444f24b2d2b00ba2989dcf78aa9bb548222dc
seunghyun <51603029+kimu2370@users.noreply.github.com>
2020-02-24 19:58:14 +0900

    update init setting

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Flavor from "Pages/Flavor/Flavor";
+import Nav from "Components/Nav/Nav";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
+        <Nav />
         <Switch>
           <Route exact path="/" component={Flavor} />
         </Switch>
