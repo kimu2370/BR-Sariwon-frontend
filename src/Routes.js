0ea1cdf6d3d1c8f1533aba7faf52a500b3d1774f
minju_kim <minju_kim@gimminju-MacBook-Pro.local>
2020-02-26 13:57:57 +0900

    loginLayout 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Flavor from "Pages/Flavor/Flavor";
+// import Flavor from "Pages/Flavor/Flavor";
 import Login from "Pages/Login/Login";
 
 class Routes extends React.Component {
@@ -8,8 +8,8 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Flavor} />
-          <Route exact path="/login" component={Login} />
+          {/* <Route exact path="/" component={Flavor} /> */}
+          <Route exact path="/" component={Login} />
         </Switch>
       </Router>
     );
