c9cb3bcac53cea4c55fa3ddcc23b7708ee896517
seunghyun <51603029+kimu2370@users.noreply.github.com>
2020-02-27 20:28:26 +0900

    공통 파일 수정 favicon and Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Flavor from "Pages/Flavor/Flavor";
 import Nav from "Components/Nav/Nav";
+import Flavor from "Pages/Flavor/Flavor";
 import Login from "Pages/Login/Login";
 
-class Routes extends React.Component {
+class Routes extends Component {
   render() {
     return (
       <Router>
         <Nav />
         <Switch>
-          {/* <Route exact path="/" component={Flavor} /> */}
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={Flavor} />
+          <Route exact path="/login" component={Login} />
         </Switch>
       </Router>
     );
