b3f44c81296f8a50127d0c0bff4faa56c78bfe9d
minju_kim <minju_kim@gimminju-MacBook-Pro.local>
2020-03-04 21:03:33 +0900

    made new ItemList component

---
+++ src/Pages/ItemList/ItemList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./itemList.scss";
+
+export class ItemList extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ItemList;
